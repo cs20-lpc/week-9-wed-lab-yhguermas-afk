1d995d88a33c2ceef9c2de174f4677ba5311ff8c
yhguermas-afk <yhguermas@zonemail.clpccd.edu>
2026-03-24 22:16:54 -0700

    Update main1.cpp

--- main1.cpp
+++ main1.cpp
@@ -3,8 +3,10 @@
 #include <ctime>
 
 int main() {
+    //created a queue of 10 integers
     ArrayQueue<int> queueA(10);
 
+    //called front() and back () on an empty queue and caught exceptions
     try {
         cout << queueA.front() << " ";
     } catch (string& e) {
@@ -16,12 +18,14 @@ int main() {
         cout << e << " ";
     }
 
+    //inserted an element(-5 in this case) and checks length front and back
     queueA.enqueue(-5);
 
     cout << "queue A length: " << queueA.getLength() << " ";
     cout << "queue A front: "  << queueA.front()     << " ";
     cout << "queue A back: "   << queueA.back()      << " ";
 
+    //dequeues an element and tests empty queue 
     queueA.dequeue();
 
     cout << "queue A length: " << queueA.getLength() << " ";
@@ -36,6 +40,7 @@ int main() {
         cout << e << " ";
     }
 
+    //filled the queue completley anc checks isFull()
     for (int i = 0; i < queueA.getMaxSize(); i++) {
         queueA.enqueue(i);
     }
@@ -48,9 +53,12 @@ int main() {
     cout << "queue A front: "  << queueA.front()     << " ";
     cout << "queue A back: "   << queueA.back()      << " ";
 
+    //copy constructor which createss queueB as a deep copy of queueA 
     ArrayQueue<int> queueB = queueA;
 
     cout << "queue A contains: ";
+
+    //dequeued all elements from queueA, which prints and empties it completely 
     for (int i = 0; i < queueA.getMaxSize(); i++) {
         cout << queueA.front() << ' ';
         queueA.dequeue();
@@ -61,19 +69,23 @@ int main() {
         cout << "queue A is now empty ";
     }
 
+    //checked if the queueB is indpendent of queueA
     cout << "queue B length: " << queueB.getLength() << " ";
     cout << "queue B front: "  << queueB.front()     << " ";
     cout << "queue B back: "   << queueB.back()      << " ";
 
+    //tested assignment operator
     ArrayQueue<int> queueC;
     queueC = queueB;
 
     queueB.clear();
 
+    //clear queueB
     if (queueB.isEmpty()) {
         cout << "queue B is now empty ";
     }
 
+    //checks if queueC still has data after queueB was cleared
     cout << "queue C length: " << queueC.getLength() << " ";
     cout << "queue C front: "  << queueC.front()     << " ";
     cout << "queue C back: "   << queueC.back()      << " ";
